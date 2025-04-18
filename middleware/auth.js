ad1b5fa8ba70e996d8422933011e5b79d2ea5961
Sam Ghoreishi <sam20gh@gmail.com>
2025-04-18 19:05:17 +0100

    Auth.js uodate to accept x-access-token

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,12 +8,18 @@ module.exports = async (req, res, next) => {
     const apiKey = req.headers['x-api-key'];
     if (apiKey === ADMIN_KEY) return next();
 
-    const authHeader = req.headers['authorization'];
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    let token = null;
+
+    if (req.headers['authorization']?.startsWith('Bearer ')) {
+        token = req.headers['authorization'].split(' ')[1];
+    } else if (req.headers['x-access-token']) {
+        token = req.headers['x-access-token'];
+    }
+
+    if (!token) {
         return res.status(401).json({ message: 'Unauthorized: Missing token' });
     }
 
-    const token = authHeader.split(' ')[1];
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET, {
