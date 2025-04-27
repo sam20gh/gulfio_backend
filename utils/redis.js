bc82d39b7f099d0d9ecf657723d8fe2fa3441800
Sam Ghoreishi <sam20gh@gmail.com>
2025-04-27 23:28:47 +0100

    fix: update Redis configuration to support SSL/TLS connection on Render

--- utils/redis.js
+++ utils/redis.js
@@ -1,10 +1,13 @@
-// utils/redis.js
 const Redis = require('ioredis');
 
 const redis = new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-    tls: {} // ← important! because Render uses SSL/TLS on Redis connection
+    tls: {
+        servername: process.env.REDIS_HOST, // <-- ADD THIS
+        rejectUnauthorized: false, // make SSL handshake loose (recommended on Render internal)
+    },
+    connectTimeout: 10000, // optional but safe
 });
 
 module.exports = redis;
