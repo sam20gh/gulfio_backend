f2169c5bc7f273132b3941f3bafa1cee33c9b04d
Sam Ghoreishi <sam20gh@gmail.com>
2025-04-28 00:22:57 +0100

    fix: add servername to Redis TLS configuration for improved connection security

--- utils/redis.js
+++ utils/redis.js
@@ -4,11 +4,9 @@ const redis = new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     tls: {
-        servername: process.env.REDIS_HOST, // <-- ADD THIS
         rejectUnauthorized: false, // make SSL handshake loose (recommended on Render internal)
     },
-    connectTimeout: 10000, // optional but safe
-    keepAlive: 1000,
+    connectTimeout: 10000,
 });
 
 module.exports = redis;
