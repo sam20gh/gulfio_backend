71e5ee27e53ff1a5810054595cdcb69242a4171f
Sam Ghoreishi <sam20gh@gmail.com>
2025-04-28 01:05:29 +0100

    refactor: streamline Redis connection setup by using a single URL configuration

--- utils/redis.js
+++ utils/redis.js
@@ -1,14 +1,7 @@
 const Redis = require('ioredis');
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    tls: {
-        rejectUnauthorized: false,
-    },
-    connectTimeout: 10000, // optional
-    maxRetriesPerRequest: 1, // 👈 prevent endless retries
-    retryStrategy: () => null, // 👈 no auto-retry
+const redis = new Redis(process.env.REDIS_URL, {
+    tls: {} // Important for Render's SSL Redis
 });
 
 module.exports = redis;
