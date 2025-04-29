d3bbf5c61f4176a21cd1816db7a8c0eaedd73fe1
Sam Ghoreishi <sam20gh@gmail.com>
2025-04-29 16:45:33 +0100

    refactor: comment out TLS configuration in Redis setup

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,6 @@
 const Redis = require('ioredis');
 
 const redis = new Redis(process.env.REDIS_URL, {
-    // tls: {
-    //     rejectUnauthorized: false,
-    // },
     connectTimeout: 10000,
     maxRetriesPerRequest: 0,
     enableOfflineQueue: false,
@@ -12,3 +9,5 @@ const redis = new Redis(process.env.REDIS_URL, {
 redis.on('error', (err) => {
     console.error('Redis error:', err);
 });
+
+module.exports = redis;
