93d8a8caceb8f4d2312188b98ad9b0413712ece8
Sam Ghoreishi <sam20gh@gmail.com>
2025-04-29 16:29:45 +0100

    refactor: remove unnecessary line breaks in redis connection setup

--- utils/redis.js
+++ utils/redis.js
@@ -1,3 +1,5 @@
+const Redis = require('ioredis');
+
 const redis = new Redis(process.env.REDIS_URL, {
     tls: {
         rejectUnauthorized: false,
@@ -10,5 +12,3 @@ const redis = new Redis(process.env.REDIS_URL, {
 redis.on('error', (err) => {
     console.error('Redis error:', err);
 });
-
-module.exports = redis;
