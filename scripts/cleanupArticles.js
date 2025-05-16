0c3063cb6d405665c6605883726ce63b17d19070
Sam Ghoreishi <sam20gh@gmail.com>
2025-05-16 14:56:36 +0100

    fix: correct MongoDB connection URI variable name in cleanup script

--- scripts/cleanupArticles.js
+++ scripts/cleanupArticles.js
@@ -5,7 +5,7 @@ const Article = require('../models/Article');
 const { clearArticlesCache } = require('../utils/cache');
 
 async function runCleanup() {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
