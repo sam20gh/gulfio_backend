6851fcbdba854a9eaff59c438cf0036a01121787
Sam Ghoreishi <sam20gh@gmail.com>
2025-05-18 18:16:32 +0100

    refactor: simplify fetchReels function to use Instagram's JSON endpoint

--- scraper/instagramReels.js
+++ scraper/instagramReels.js
@@ -1,49 +1,30 @@
-
 // scrape/instagramReels.js
 const axios = require('axios');
-const cheerio = require('cheerio');
 const Reel = require('../models/Reel');
 
 /**
- * Fetches the raw list of reels for a given Instagram username
- * by parsing the window._sharedData blob.
- *
- * @param {string} username
- * @returns {Promise<Array<{ reelId: string, videoUrl: string }>>}
+ * Fetches reels by hitting Instagram’s JSON endpoint.
  */
 async function fetchReels(username) {
-    const profileUrl = `https://www.instagram.com/${username}/`;
-    const { data: html } = await axios.get(profileUrl, {
+    // This endpoint returns JSON with the same data we need.
+    const jsonUrl = `https://www.instagram.com/${username}/?__a=1&__d=dis`;
+    const { data } = await axios.get(jsonUrl, {
         headers: { 'User-Agent': 'Mozilla/5.0' }
     });
 
-    const m = html.match(
-        /<script type="text\/javascript">window\._sharedData = (.+?);<\/script>/
-    );
-    if (!m) throw new Error('Could not find sharedData on profile page');
-
-    const sharedData = JSON.parse(m[1]);
-    const edges = sharedData
-        .entry_data.ProfilePage[0]
-        .graphql.user
-        .edge_owner_to_timeline_media.edges;
-
+    // Dive into the JSON:
+    const edges = data.graphql.user.edge_owner_to_timeline_media.edges;
     return edges
         .filter(e => e.node.__typename === 'GraphVideo')
-        .map(e => ({ reelId: e.node.id, videoUrl: e.node.video_url }));
+        .map(e => ({
+            reelId: e.node.id,
+            videoUrl: e.node.video_url
+        }));
 }
 
-/**
- * Scrapes reels for one Source, upserting into the DB.
- *
- * @param {ObjectId} sourceId
- * @param {string} username
- * @returns {Promise<Array<Reel>>}  the newly-upserted Reel docs
- */
 async function scrapeReelsForSource(sourceId, username) {
     const reels = await fetchReels(username);
     const results = [];
-
     for (const { reelId, videoUrl } of reels) {
         const reel = await Reel.findOneAndUpdate(
             { source: sourceId, reelId },
@@ -52,7 +33,6 @@ async function scrapeReelsForSource(sourceId, username) {
         );
         results.push(reel);
     }
-
     return results;
 }
 
