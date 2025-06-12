6831d774654e5b6d32062b4505b92a92d8c9b512
Sam Ghoreishi <sam20gh@gmail.com>
2025-06-13 00:06:48 +0100

    refactor: streamline migration process for social fields in reels

--- scripts/migrateAddSocialFieldsToReels.js
+++ scripts/migrateAddSocialFieldsToReels.js
@@ -1,49 +1,27 @@
 const mongoose = require('mongoose');
 const Reel = require('../models/Reel');
-
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/YOUR_DB_NAME';
 
-async function migrate() {
+async function run() {
     await mongoose.connect(MONGO_URI);
 
-    const reels = await Reel.find();
-    let updated = 0;
+    const updates = [
+        Reel.updateMany({ likes: { $exists: false } }, { $set: { likes: 0 } }),
+        Reel.updateMany({ likedBy: { $exists: false } }, { $set: { likedBy: [] } }),
+        Reel.updateMany({ dislikes: { $exists: false } }, { $set: { dislikes: 0 } }),
+        Reel.updateMany({ dislikedBy: { $exists: false } }, { $set: { dislikedBy: [] } }),
+        Reel.updateMany({ viewCount: { $exists: false } }, { $set: { viewCount: 0 } }),
+        Reel.updateMany({ viewedBy: { $exists: false } }, { $set: { viewedBy: [] } }),
+    ];
 
-    for (const reel of reels) {
-        let changed = false;
-
-        // Use "in" to check for presence in raw doc, NOT Mongoose virtuals/defaults
-        if (!Object.prototype.hasOwnProperty.call(reel.toObject(), 'likes')) {
-            reel.likes = 0; changed = true;
-        }
-        if (!Object.prototype.hasOwnProperty.call(reel.toObject(), 'likedBy')) {
-            reel.likedBy = []; changed = true;
-        }
-        if (!Object.prototype.hasOwnProperty.call(reel.toObject(), 'dislikes')) {
-            reel.dislikes = 0; changed = true;
-        }
-        if (!Object.prototype.hasOwnProperty.call(reel.toObject(), 'dislikedBy')) {
-            reel.dislikedBy = []; changed = true;
-        }
-        if (!Object.prototype.hasOwnProperty.call(reel.toObject(), 'viewCount')) {
-            reel.viewCount = 0; changed = true;
-        }
-        if (!Object.prototype.hasOwnProperty.call(reel.toObject(), 'viewedBy')) {
-            reel.viewedBy = []; changed = true;
-        }
-
-        if (changed) {
-            await reel.save();
-            updated++;
-            console.log(`Updated reel: ${reel._id}`);
-        }
-    }
-
-    console.log(`Migration complete. Updated ${updated} reel(s).`);
+    const results = await Promise.all(updates);
+    results.forEach((result, idx) => {
+        console.log(`Updated ${result.modifiedCount || result.nModified} reels for update #${idx + 1}`);
+    });
     mongoose.disconnect();
 }
 
-migrate().catch(err => {
+run().catch(err => {
     console.error('Migration error:', err);
     mongoose.disconnect();
 });
