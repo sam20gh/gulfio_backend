f0f43e560f3d40071b65736e0fd570410764cb47
Sam Ghoreishi <sam20gh@gmail.com>
2025-06-15 20:23:01 +0100

    fix: update caption field migration to set null instead of empty array

--- scripts/migrateAddSocialFieldsToReels.js
+++ scripts/migrateAddSocialFieldsToReels.js
@@ -12,7 +12,7 @@ async function run() {
         Reel.updateMany({ dislikedBy: { $exists: false } }, { $set: { dislikedBy: [] } }),
         Reel.updateMany({ viewCount: { $exists: false } }, { $set: { viewCount: 0 } }),
         Reel.updateMany({ viewedBy: { $exists: false } }, { $set: { viewedBy: [] } }),
-        Reel.updateMany({ caption: { $exists: false } }, { $set: { caption: [] } }),
+        Reel.updateMany({ caption: { $exists: false } }, { $set: { caption: null } }),
         Reel.updateMany({ embedding: { $exists: false } }, { $set: { embedding: [] } }),
     ];
 
