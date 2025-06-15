68a5674e550eb715ca2f50c876a0af65a3c36dbd
Sam Ghoreishi <sam20gh@gmail.com>
2025-06-15 20:12:09 +0100

    feat: add embedding field to Reel model

--- models/Reel.js
+++ models/Reel.js
@@ -13,7 +13,11 @@ const ReelSchema = new Schema({
     dislikes: { type: Number, default: 0 },
     dislikedBy: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
     viewCount: { type: Number, default: 0 },
-    viewedBy: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }]
+    viewedBy: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
+    embedding: {
+        type: [Number],
+        default: [],
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Reel', ReelSchema);
