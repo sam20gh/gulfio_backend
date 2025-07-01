383ba0d3bdaa0448e90a6b6eb85708b0a2ea38a2
Sam Ghoreishi <sam20gh@gmail.com>
2025-07-01 19:02:36 +0100

    feat: rename and relocate scrapeYouTubeShortsForSource function to youtubeShortsScraper.js

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -10,7 +10,7 @@ const { scrapeReelsForSource } = require('./instagramReels');
 const scrapeUaeLottoResults = require('./lottoscrape');
 const LottoResult = require('../models/LottoResult');
 const { getDeepSeekEmbedding } = require('../utils/deepseek');
-const { scrapeYouTubeShortsForSource } = require('./youtubeShorts');
+const { scrapeYouTubeShortsForSource } = require('./youtubeShortsSxraper.js');
 
 async function scrapeAllSources(frequency = null) {
     let sources = await Source.find();
